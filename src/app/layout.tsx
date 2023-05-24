86fa6bbb9c32de9b50f73315be1312bb11696976
Darren Wong <wongdarren1997@gmail.com>
2023-05-23 20:10:02 -0500

    Added template Hero component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import React from 'react';
 import { Inter } from 'next/font/google';
-import { Container, Footer, Navbar } from '@/components';
+import { Footer, Hero } from '@/components';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,11 +14,10 @@ function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Container>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-        </Container>
+        {/*<Navbar />*/}
+        <Hero />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Container from '@/components/Container';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
 
-export { Container, Footer, Navbar };
+export { Container, Footer, Navbar, Hero };
